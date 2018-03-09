e44801e116b126e2ebedbc6841e13eb471582265
Adam Sven Johnson <adam@pkqk.net>
2018-03-09 15:41:54 +1300

    Day 8 part 1 solution

--- day8/day8.go
+++ day8/day8.go
@@ -14,37 +14,86 @@ func init() {
 	// days.Register("8b", Part2)
 }
 
-type instruction struct {
+type Instruction struct {
 	register, condition, check_register string
-	inc, amount, check_value int
+	amount, check_value int
+}
+
+func (instr Instruction) Execute(registers map[string]int) {
+	run := false
+	switch instr.condition {
+	case ">":
+		run = registers[instr.check_register] > instr.check_value
+	case "<":
+		run = registers[instr.check_register] < instr.check_value
+	case ">=":
+		run = registers[instr.check_register] >= instr.check_value
+	case "<=":
+		run = registers[instr.check_register] <= instr.check_value
+	case "==":
+		run = registers[instr.check_register] == instr.check_value
+	case "!=":
+		run = registers[instr.check_register] != instr.check_value
+	default:
+		fmt.Println("Missing operation", instr.condition)
+		panic("Missing operation")
+	}
+	if run {
+		registers[instr.register] += instr.amount
+	}
 }
 
 func Part1(path string) {
 	input, _ := os.Open(path)
 	instructions := readInstructions(input)
 	registers := runMachine(instructions)
-
-	fmt.Printf("%d\n", registers)
+	maxRegister(registers)
 }
 
-func readInstructions(input *os.File) []instruction {
-	re := regexp.MustCompile(`(\w+) (inc|dec) ((-)?\d+) if (\w+) ([><=]+) ((-)?\d+)`)
-	instructions := make([]instruction, 0)
+func maxRegister(registers map[string]int) {
+	var key string
+	var max int
+	for reg, val := range registers {
+		key = reg
+		max = val
+		break
+	}
+	for reg, val := range registers {
+		if val > max {
+			max = val
+			key = reg
+		}
+	}
+	fmt.Println("Max register", key, max)
+}
+
+func readInstructions(input *os.File) []Instruction {
+	re := regexp.MustCompile(`(\w+) (inc|dec) ((-)?\d+) if (\w+) ([><!=]+) ((-)?\d+)`)
+	instructions := make([]Instruction, 0)
 	scanner := bufio.NewScanner(bufio.NewReader(input))
 	for scanner.Scan() {
 		line := scanner.Text()
 		matches := re.FindStringSubmatch(line)
-		fmt.Println(line)
-		register := matches[1]
-		inc, _ := strconv.Atoi(matches[3])
+		amount, _ := strconv.Atoi(matches[3])
 		if matches[2] == "dec" {
-			inc = -inc
+			amount = -amount
 		}
-		fmt.Println(register, inc)
+		check_value, _ := strconv.Atoi(matches[7])
+		instructions = append(instructions, Instruction{
+			register: matches[1],
+			amount: amount,
+			check_register: matches[5],
+			condition: matches[6],
+			check_value: check_value,
+		})
 	}
 	return instructions
 }
 
-func runMachine(instructions []instruction) map[string]int {
-	return make(map[string]int)
+func runMachine(instructions []Instruction) map[string]int {
+	registers := make(map[string]int)
+	for _, instr := range instructions {
+		instr.Execute(registers)
+	}
+	return registers
 }
