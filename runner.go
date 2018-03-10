b1b7600505344ee5262abee20d142e5a5ca0b4e6
Adam Sven Johnson <adam@pkqk.net>
2018-03-10 17:02:51 +1300

    go fmt

--- runner.go
+++ runner.go
@@ -1,26 +1,26 @@
 package main
 
 import (
-  "days"
-  "fmt"
-  "os"
-  _ "day1"
-  _ "day2"
-  _ "day3"
-  _ "day4"
-  _ "day5"
-  _ "day6"
-  _ "day7"
-  _ "day8"
-  _ "day9"
+	_ "day1"
+	_ "day2"
+	_ "day3"
+	_ "day4"
+	_ "day5"
+	_ "day6"
+	_ "day7"
+	_ "day8"
+	_ "day9"
+	"days"
+	"fmt"
+	"os"
 )
 
 func main() {
-  if len(os.Args) < 2 {
-    fmt.Println("usage: go run runner.go n input")
-    os.Exit(1)
-  }
-  day, input := os.Args[1], os.Args[2]
-  fmt.Println("Advent of Code - Day", day)
-  days.Call(day, input)
+	if len(os.Args) < 2 {
+		fmt.Println("usage: go run runner.go n input")
+		os.Exit(1)
+	}
+	day, input := os.Args[1], os.Args[2]
+	fmt.Println("Advent of Code - Day", day)
+	days.Call(day, input)
 }
